13cb97e9a4c7337b132c1edcded3323497c91593
Aygun <sahmuradova18@gmail.com>
2024-03-12 12:12:28 +0400

    LabTask

--- Program.cs
+++ Program.cs
@@ -6,122 +6,162 @@ namespace GitPull_Merge
 	{
 		static void Main(string[] args)
 		{
-			#region Task1
-			//Task - 1 
-			//Verilmiş array-də tək ədədlərin sayını tapmaq və çap etmək.
+            //#region Task1
+            ////Task - 1 
+            ////Verilmiş array-də tək ədədlərin sayını tapmaq və çap etmək.
 
-			int[] arr1 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
-			int count = 0;
+            //int[] arr1 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
+            //int count = 0;
 
-			for (int i = 0; i < arr1.Length; i++)
-			{
-				if (arr1[i] % 2 != 0)
-				{
-					count++;
-				}
-			}
-			Console.WriteLine(count);
-			#endregion
-			#region Task2
-			//Task - 2 
-			//Verilmiş array-də ən kiçik və ən böyük ədədlərin cəmini tapmaq. 
-			int[] arr2 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
-			int min = arr2[0];
-			int max = arr2[0];
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //	if (arr1[i] % 2 != 0)
+            //	{
+            //		count++;
+            //	}
+            //}
+            //Console.WriteLine(count);
+            //#endregion
+            //#region Task2
+            ////Task - 2 
+            ////Verilmiş array-də ən kiçik və ən böyük ədədlərin cəmini tapmaq. 
+            //int[] arr2 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
+            //int min = arr2[0];
+            //int max = arr2[0];
 
-			for (int i = 0; i < arr2.Length; i++)
-			{
-				if (arr2[i]<min)
-				{
-					min = arr2[i];
-				}
-				if (arr2[i] > max) 
-				{ 
-					max = arr2[i];
-				}
-			}
-			int sum = min + max;
-            Console.Write("En kicik eded: ");
-            Console.WriteLine(min);
-            Console.Write("En boyuk eded: ");
-            Console.WriteLine(max);
-            Console.Write("Ededlerin cemi: ");
-            Console.WriteLine(sum);
-			#endregion
-			#region Task3
-			//Task - 3
-			//Verilmiş array-dəki ədədlərin ən böyük və ən kiçik fərqlərini tapmaq. 
-			int[] arr3 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
-			int minNum = arr3[0];
-			int maxNum = arr3[0];
+            //for (int i = 0; i < arr2.Length; i++)
+            //{
+            //	if (arr2[i]<min)
+            //	{
+            //		min = arr2[i];
+            //	}
+            //	if (arr2[i] > max) 
+            //	{ 
+            //		max = arr2[i];
+            //	}
+            //}
+            //int sum = min + max;
+            //         Console.Write("En kicik eded: ");
+            //         Console.WriteLine(min);
+            //         Console.Write("En boyuk eded: ");
+            //         Console.WriteLine(max);
+            //         Console.Write("Ededlerin cemi: ");
+            //         Console.WriteLine(sum);
+            //#endregion
+            //#region Task3
+            ////Task - 3
+            ////Verilmiş array-dəki ədədlərin ən böyük və ən kiçik fərqlərini tapmaq. 
+            //int[] arr3 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
+            //int minNum = arr3[0];
+            //int maxNum = arr3[0];
 
-			for (int i = 0; i < arr3.Length; i++)
-			{
-				if (arr3[i] < minNum)
-				{
-					minNum = arr3[i];
-				}
-				if (arr3[i] > maxNum)
-				{
-					maxNum = arr3[i];
-				}
-			}
-			int difference= max-min;
-			Console.Write("En boyuk eded: ");
-			Console.WriteLine(max);
-			Console.Write("En kicik eded: ");
-			Console.WriteLine(min);
-			Console.Write("Ededlerin ferqi: ");
-			Console.WriteLine(difference);
-			#endregion
-			#region Task4
-			//Task - 4
-           //Verilmiş array-dəki ədədlərin sırasını artan sıraya salmaq.
-			int[] arr4 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
-			int temp=0;
+            //for (int i = 0; i < arr3.Length; i++)
+            //{
+            //	if (arr3[i] < minNum)
+            //	{
+            //		minNum = arr3[i];
+            //	}
+            //	if (arr3[i] > maxNum)
+            //	{
+            //		maxNum = arr3[i];
+            //	}
+            //}
+            //int difference= max-min;
+            //Console.Write("En boyuk eded: ");
+            //Console.WriteLine(max);
+            //Console.Write("En kicik eded: ");
+            //Console.WriteLine(min);
+            //Console.Write("Ededlerin ferqi: ");
+            //Console.WriteLine(difference);
+            //#endregion
+            //#region Task4
+            ////Task - 4
+            //        //Verilmiş array-dəki ədədlərin sırasını artan sıraya salmaq.
+            //int[] arr4 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
+            //int temp=0;
 
-			for (int i = 0; i < arr4.Length; i++)
-			{
-				for (int j= i+1; j < arr4.Length; j++)
-				{
-					if (arr4[i] > arr4[j])
-					{
-						temp = arr4[i];
-						arr4[i] = arr4[j];
-						arr4[j] = temp;
-					}
-				}
-			}
-            Console.WriteLine("Artan sira ile array:");
-            foreach (int num in arr4)
-			{
-                Console.WriteLine(num);
+            //for (int i = 0; i < arr4.Length; i++)
+            //{
+            //	for (int j= i+1; j < arr4.Length; j++)
+            //	{
+            //		if (arr4[i] > arr4[j])
+            //		{
+            //			temp = arr4[i];
+            //			arr4[i] = arr4[j];
+            //			arr4[j] = temp;
+            //		}
+            //	}
+            //}
+            //         Console.WriteLine("Artan sira ile array:");
+            //         foreach (int num in arr4)
+            //{
+            //             Console.WriteLine(num);
+            //         }
+
+
+            //#endregion
+            //#region Task5
+            ////Task - 5
+            ////Verilmiş array-dəki tək və cüt ədədlərin cəmini ayrı-ayrı tapmaq.
+            //int[] arr5 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
+            //int sum1 = 0;
+            //int sum2 = 0;
+
+            //for (int i = 0;i < arr4.Length;i++)
+            //{
+            //	if (arr4[i] % 2 == 0)
+            //	{
+            //		sum1 += arr4[i];
+
+            //             }
+            //	else
+            //	{
+
+            //		sum2 += arr4[i];
+            //	}
+            //}
+            //Console.WriteLine("Cut ededlerin cemi: " + sum1);
+            //Console.WriteLine("Tek ededlerin cemi: " + sum2);
+            //#endregion
+
+            #region 
+            //Bir array yaradin,o arraydaki ededlerin cemini ve ededi ortasini tapin
+            //int[] arr6= {1,2,3,4,5,6 };
+            //int sum3 = 0;
+            //int count1 = 0;
+
+            //for (int i = 0; i < arr6.Length; i++)
+            //{
+            //	sum3 += arr6[i];
+            //	count1++;
+            //}
+            //         Console.WriteLine(sum3/count1);
+            #endregion
+            #region
+            
+            Console.WriteLine("x1'i daxil edin: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1'i daxil edin: ");
+			int y1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("x2'i daxil edin: ");
+			int x2= Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("y2'i daxil edin: ");
+			int y2 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("x3'i daxil edin: ");
+			int x3 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("y3'i daxil edin: ");
+			int y3= Convert.ToInt32(Console.ReadLine());
+
+            if ((y2-y1) / (x2- x1) == (y3 - y2) / (x3-x2)&&(y3 - y2) / (x3 - x2)==(y3-y1)/(x3-x1))
+            {
+                Console.WriteLine("Noqteler bir duz xett uzerindedir");
             }
-
-
-			#endregion
-			#region Task5
-			//Task - 5
-			//Verilmiş array-dəki tək və cüt ədədlərin cəmini ayrı-ayrı tapmaq.
-			int[] arr5 = { 12, 23, 34, 54, 56, 76, 78, 11, 15 };
-			int sum1 = 0;
-			int sum2 = 0;
-
-			for (int i = 0;i < arr4.Length;i++)
-			{
-				if (arr4[i] % 2 == 0)
-				{
-					sum1 += arr4[i];
-                    
-                }
-				if (arr4[i] % 2 != 0)
-				{
-					
-					sum2 += arr4[i];
-				}
-			}
-			Console.WriteLine("Cut ededlerin cemi: " + sum1);
-			Console.WriteLine("Tek ededlerin cemi: " + sum2);
+            else
+            {
+                Console.WriteLine("Noqteler bir duz xett uzerinde deyil");
+            }
 
 
 			#endregion
